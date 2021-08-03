05574b439b2214759bd82c9624b0c75d52f2f69a
k-nishikawa <k-nishikawa@atware.co.jp>
2021-08-03 10:23:18 +0900

    using sass

---
+++ src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-form',
+  templateUrl: './simple-form.component.html',
+  styleUrls: ['./simple-form.component.sass']
+})
+export class SimpleFormComponent implements OnInit {
+  text1: string = "";
+  text2: string = "";
+  result: string = "Let's add two numbers";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addAndShow(): void {
+    let forResult: string = "Please input validate value.";
+    let int1: number;
+    let int2: number;
+    int1 = Number(this.text1);
+    int2 = Number(this.text2);
+    
+    if (!Number.isNaN(int1) && !Number.isNaN(int2)) {
+      forResult = `${int1} + ${int2} = ${int1 + int2}`;
+    }
+    this.result = forResult;
+  }
+
+}
