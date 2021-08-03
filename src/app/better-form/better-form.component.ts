8d0fb4482a5f8c7440f68a4f93889e78967a58a8
k-nishikawa <k-nishikawa@atware.co.jp>
2021-08-03 18:27:15 +0900

    update: 7章終了

--- src/app/better-form/better-form.component.ts
+++ src/app/better-form/better-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormGroup,FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-better-form',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./better-form.component.scss']
 })
 export class BetterFormComponent implements OnInit {
+  calcForm: FormGroup = new FormGroup({});
+  result: string = "Let's add numbers;"
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.calcForm = new FormGroup({
+      "fieldOne": new FormControl("0", Validators.required),
+      "fieldTwo": new FormControl("0", [Validators.required, Validators.maxLength(5)]),
+    });
   }
 
+  get fieldOne() { return this.calcForm.get("fieldOne");}
+  get fieldTwo() { return this.calcForm.get("fieldTwo"); }
+  
+  addAnyway() {
+    let text1: string = this.fieldOne?.value;
+    let text2: string = this.fieldTwo?.value;
+    let resultStr: string = "";
+
+    if (Number.isNaN(Number(text1)) || Number.isNaN(Number(text2))) {
+      resultStr = text1 + text2;
+    } else {
+      resultStr = `${text1} + ${text2} = ${Number(text1) + Number(text2)}`;
+    }
+    this.result = resultStr;
+  }
+
+  clearResult() {
+    this.result = "";
+  }
 }
