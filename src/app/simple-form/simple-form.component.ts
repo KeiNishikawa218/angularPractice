2f27257043a3ea22d05b9a1a29d161b4509712d6
k-nishikawa <k-nishikawa@atware.co.jp>
2021-08-03 10:48:32 +0900

    use scss

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   templateUrl: './simple-form.component.html',
-  styleUrls: ['./simple-form.component.sass']
+  styleUrls: ['./simple-form.component.scss']
 })
 export class SimpleFormComponent implements OnInit {
   text1: string = "";
