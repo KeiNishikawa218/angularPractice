7e24f622a90ed07623b0496b09094d0d3f1732cf
k-nishikawa <k-nishikawa@atware.co.jp>
2021-08-04 12:55:01 +0900

    finish chapter 8

--- src/app/controls/controls.component.ts
+++ src/app/controls/controls.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, CheckboxControlValueAccessor } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-controls',
@@ -8,15 +9,34 @@ import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 })
 export class ControlsComponent implements OnInit {
   coffeeForm: FormGroup;
+  hotcoldsel = ["Hot", "Cold"];
+  addssel = ["Milk", "Sugar"];
+  nutsel = ["peanut", "almond", "walnut"]
 
   constructor(private fb: FormBuilder) {
     this.coffeeForm = this.fb.group({
       name: "blend",
-      taste: "mild with good balance"
+      taste: "mild with good balance",
+      hotcold: this.hotcoldsel[0],
+      adds: this.fb.array([]),
+      nut: this.nutsel[0]
     })
   }
 
   ngOnInit(): void {
   }
 
+  // Reference: https://github.com/angular/components/issues/1142
+  onCheckChanged(item: string, e: Event) {
+    const checkbox = e.target as HTMLInputElement;
+
+    let formArray = <FormArray>this.coffeeForm.controls.adds;
+    if (checkbox.checked) {
+      formArray.push(new FormControl(item));
+    } else {
+      let index = formArray.controls.findIndex(elm => elm.value == item)
+      formArray.removeAt(index);
+    }
+  }
 }
+
